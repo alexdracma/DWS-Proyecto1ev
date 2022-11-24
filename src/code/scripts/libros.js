1f33333f9fefb2decaaeb284d76944725140cb09
alexdracma <alexdracma@gmail.com>
2022-11-24 17:17:43 +0100

    decorada paginación libros

--- src/code/scripts/libros.js
+++ src/code/scripts/libros.js
@@ -7,5 +7,15 @@ const dataTable = new simpleDatatables.DataTable("#librosTable", {
     }
 })
 
+dataTable.on('datatable.init', decoreActive)
+dataTable.on('datatable.page', decoreActive)
+
+function decoreActive() {
+    const active = document.querySelector('ul.dataTable-pagination-list').querySelector('.active').firstElementChild
+    active.style.color = '#fff'
+    active.style.backgroundColor = '#7452c5'
+}
+
 const select = document.querySelector('.dataTable-selector')
-    select.classList.add('form-select')
+select.classList.add('form-select')
+
